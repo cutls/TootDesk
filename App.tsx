67be72609b6075aed1d277b8a040deebfa0b4df8
cutls <web-pro@cutls.com>
2023-01-25 01:37:01 +0900

    notification on foreground

--- App.tsx
+++ App.tsx
@@ -27,6 +27,14 @@ const LinkingConfiguration = {
 		},
 	},
 }
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false
+  }),
+})
+
 export default function App() {
 	React.useEffect(() => {
 		const subscription = Notifications.addNotificationResponseReceivedListener(response => {
