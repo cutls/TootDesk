ead3e48fe88dac6364d86164f4f4e8b09a704d72
cutls <web-pro@cutls.com>
2023-01-29 00:44:47 +0900

    i18n bug

--- utils/i18n.ts
+++ utils/i18n.ts
@@ -2,9 +2,10 @@ import * as Localization from 'expo-localization'
 import { I18n } from 'i18n-js'
 import en from '../i18n/en.json'
 const i18n = new I18n({ en })
-i18n.locale = Localization.getLocales()[0].languageCode
+const langCode = Localization.getLocales()[0].languageCode
+i18n.locale = langCode
 i18n.missingTranslation.register('jaFallback',(i18n, scope, options) => scope.toString())
 i18n.missingTranslation.register('enFallback',(i18n, scope, options) => i18n.translations.en[scope.toString()])
-i18n.missingBehavior = 'enFallback'
+i18n.missingBehavior = langCode === 'ja' ? 'jaFallback' : 'enFallback'
 
 export default i18n
