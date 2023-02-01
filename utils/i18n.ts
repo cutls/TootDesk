89e888ec19f7f96cae3e657c1ce56e79e08f6a12
cutls <web-pro@cutls.com>
2023-02-01 10:03:56 +0900

    i18n replace bug

--- utils/i18n.ts
+++ utils/i18n.ts
@@ -1,11 +1,18 @@
 import * as Localization from 'expo-localization'
-import { I18n } from 'i18n-js'
+import { Dict, I18n } from 'i18n-js'
 import en from '../i18n/en.json'
 const i18n = new I18n({ en })
+const replacer = (scope: string, options: Dict) => {
+    for (const [key, value] of Object.entries(options)) {
+        const keyReg = new RegExp(`%{${key}}`, 'g')
+        scope = scope.replace(keyReg, value)
+    }
+    return scope
+}
 const langCode = Localization.getLocales()[0].languageCode
 i18n.locale = langCode
-i18n.missingTranslation.register('jaFallback',(i18n, scope, options) => scope.toString())
-i18n.missingTranslation.register('enFallback',(i18n, scope, options) => i18n.translations.en[scope.toString()])
+i18n.missingTranslation.register('jaFallback',(i18n, scope, options) => replacer(scope.toString(), options))
+i18n.missingTranslation.register('enFallback',(i18n, scope, options) => replacer(i18n.translations.en[scope.toString()], options))
 i18n.missingBehavior = langCode === 'ja' ? 'jaFallback' : 'enFallback'
 
 export default i18n
